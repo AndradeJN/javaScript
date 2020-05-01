8a0e154837061b1637a846608a398db919222a2e
AndradeJN <josenetogui@hotmail.com>
2020-05-01 11:05:11 -0300

    Aula 11

---
+++ cursoemvideo/aula11/ex08.js
@@ -0,0 +1,7 @@
+var vel = 50.5
+
+console.log(`A velocidade do seu carro é: ${vel} Km/h`)
+if (vel > 60) {
+    console.log('Você está acima de 60 km/h e foi multado')
+}
+console.log('Dirija com cuidado, use sempre o cinto de segurança')
---
+++ cursoemvideo/aula11/ex09.js
@@ -0,0 +1,10 @@
+
+var pais = 'Brasil'
+
+console.log(`Live in ${pais}`)
+
+if (pais == 'Brasil') {
+    console.log('Você é Brasileiro')
+}else {
+    console.log('Você é Estrangeiro')
+}
